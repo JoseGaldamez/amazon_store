3408ff1c351fad7a54f8e2a20465dbba88e31df3
Jose Galdamez <josegaldamez1991@gmail.com>
2022-08-16 22:04:34 -0600

    adding responsive

--- amazon_store_front/src/components/Navbar/index.jsx
+++ amazon_store_front/src/components/Navbar/index.jsx
@@ -5,25 +5,10 @@ import LogoAmazon from "../../assets/img/logo.png";
 export const Navbar = ({ children }) => {
   return (
     <nav className="navbar navbar-expand-md navbar-dark fixed-top black-bg">
-      <a className="navbar-brand" href="#">
-        <img src={LogoAmazon} alt="Logo Amazon" />
-      </a>
-      <button
-        className="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarsExampleDefault"
-        aria-controls="navbarsExampleDefault"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span className="navbar-toggler-icon"></span>
-      </button>
+      <img src={LogoAmazon} alt="Logo Amazon" />
 
-      <div className="collapse navbar-collapse" id="navbarsExampleDefault">
-        <ul className="navbar-nav mr-auto"></ul>
-        {children}
-      </div>
+      <ul className="navbar-nav mr-auto"></ul>
+      {children}
     </nav>
   );
 };
